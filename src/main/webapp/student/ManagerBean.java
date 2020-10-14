3c4e5220cf01e52faf216c3beeb770ed63badd03
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-10-14 15:26:58 +0000

    Etape 02

---
+++ src/main/webapp/student/ManagerBean.java
@@ -0,0 +1,12 @@
+@javax.faces.bean.ManagedBean
+
+@ManagedBean(name="manager")
+public class ManagerBean {
+
+    private String version = "1.0-SNAPSHOT";
+
+    public String getVersion() {
+        return version;
+    }
+
+}
