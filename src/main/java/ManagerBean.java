26c568c42fbbb1ee774ee28e9c2b913749e61d8e
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-10-28 21:38:58 +0000

    test

--- src/main/java/ManagerBean.java
+++ src/main/java/ManagerBean.java
@@ -11,17 +11,27 @@ public class ManagerBean {
         return version;
     }
     
+    
+}
+
+    public class Student{
+
+        public int id;
+        public String lastname;
+        public String firstname;
+        public String datebirth;
+        public String phoneNumber;
+
     public ArrayList<Student> getStudentList(){
+    
     public List<Student> getStudentList = Arrays.asList(
+        
     new Student[] {
         new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00"),
         new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00")
-    }    
-    );}
+    }  
+    );
 }
-
-
-
-    
+}    
 
  
