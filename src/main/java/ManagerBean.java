d92ec6edb34e186feb67f2eb29a568ceca2aa224
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-10-28 19:25:40 +0000

    another commit

--- src/main/java/ManagerBean.java
+++ src/main/java/ManagerBean.java
@@ -1,6 +1,8 @@
+@java.util.ArrayList
 @javax.faces.bean.ManagedBean
+@Named("manager")
+@RequestScoped
 
-@ManagedBean(name="manager")
 public class ManagerBean {
 
     private String version = "1.0-SNAPSHOT";
@@ -8,5 +10,18 @@ public class ManagerBean {
     public String getVersion() {
         return version;
     }
-
+    
+    /*public ArrayList<Student> getStudentList(){
+    public List<Student> getStudentList = Arrays.asList(
+    new Student[] {
+        new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00"),
+        new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00")
+    }    
+    );}*/
 }
+
+
+
+    
+
+ 
