d1e92e725986c54b807d369ee78ee0f6b2b2f680
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-11-12 14:43:37 +0000

    another com

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,10 +1,26 @@
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Student{
 
-public ArrayList<Student> list = new ArrayList<>(Arrays.asList(1,"JEAN" , "Paul", "15/02/2001", "0696458511"));
+    public int id;
+    public String lastname;
+    public String firstname;
+    public String datebirth;
+    public int phoneNumber;
 
+    public Student (int id, String lastname, String firstname, String datebirth, String phoneNumber) {
+    public ArrayList<Student> getStudentList(){
+
+    public List<Student> getStudentList= Arrays.asList(
+
+    new Student [] {
+    new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00"),
+    new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00")
+    }
+);
+    }
 
 }
-
+}   
