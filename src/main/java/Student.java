4be8d72804edad70274c5de9fe9b9dff3682dcb0
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-11-13 11:51:04 +0000

    13.11 commit

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,7 @@
 import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Student{
 
@@ -11,16 +12,21 @@ public class Student{
     public int phoneNumber;
 
     public Student (int id, String lastname, String firstname, String datebirth, String phoneNumber) {
+    
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.datebirth = datebirth;
+        this.phoneNumber = phoneNumber;
+        
     public ArrayList<Student> getStudentList(){
-
+        
     public List<Student> getStudentList= Arrays.asList(
-
     new Student [] {
     new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00"),
     new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00")
     }
 );
-    }
-
 }
 }   
+}
