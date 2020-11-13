f8f0f3f20396e250d961ee6adf859978e64a2a68
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-11-13 15:00:18 +0000

    a commit

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -5,32 +5,66 @@ import java.util.List;
 import ManagerBean;
 
 
-
 public class Student{
 
-    public int id;
-    public String lastname;
-    public String firstname;
-    public String datebirth;
-    public String phoneNumber;
+    private int id;
+    private String lastname;
+    private String firstname;
+    private String datebirth;
+    private String phoneNumber; 
 
-    public Student (int id, String lastname, String firstname, String datebirth, String phoneNumber) {
-    
+    public Student(int id, String lastname, String firstname, String datebirth, String phoneNumber) {
+
         this.id = id;
         this.lastname = lastname;
         this.firstname = firstname;
         this.datebirth = datebirth;
         this.phoneNumber = phoneNumber;
-
-      
-    public ArrayList<Student> getStudentList() {
-        
-    public List<Student> getStudentList = Arrays.asList (
-    new Student []{
-    new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00"),
-    new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00")
     }
-    );
+    public ArrayList<Student> getStudentList(){
+
+        System.println(this.id, this.lastname, this.firstname, this.datebirth, this.phoneNumber);
+    }
+
+    public List<Student> setStudentList = Arrays.asList; {
+
+        new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00");
+        new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00");
+        
+    }
+
 }
-}   
+
+
+
+/*
+public class Student{
+
+    private int id;
+    private String lastname;
+    private String firstname;
+    private String datebirth;
+    private String phoneNumber; 
+
+    public Student(int id, String lastname, String firstname, String datebirth, String phoneNumber) {
+
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.datebirth = datebirth;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getStudentList(){
+
+        System.println(this.id, this.lastname, this.firstname, this.datebirth, this.phoneNumber);
+    }
+
+    public void setStudentList(){
+
+        new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00");
+        new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00");
+        
+    }
+
 }
+*/
