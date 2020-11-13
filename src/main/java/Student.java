bb5601c7d83957972b6597cc90a119a2f608da97
emelyne972 <62777736+emelyne972@users.noreply.github.com>
2020-11-13 13:14:09 +0000

    another com

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,6 +2,9 @@ import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import ManagerBean;
+
+
 
 public class Student{
 
@@ -9,7 +12,7 @@ public class Student{
     public String lastname;
     public String firstname;
     public String datebirth;
-    public int phoneNumber;
+    public String phoneNumber;
 
     public Student (int id, String lastname, String firstname, String datebirth, String phoneNumber) {
     
@@ -18,15 +21,16 @@ public class Student{
         this.firstname = firstname;
         this.datebirth = datebirth;
         this.phoneNumber = phoneNumber;
+
+      
+    public ArrayList<Student> getStudentList() {
         
-    public ArrayList<Student> getStudentList(){
-        
-    public List<Student> getStudentList= Arrays.asList(
-    new Student [] {
+    public List<Student> getStudentList = Arrays.asList (
+    new Student []{
     new Student (0, "Dupont","pierre","14/01/1996","06 96 00 00 00"),
     new Student (1, "DuBois","fred","01/01/1996","06 96 00 00 00")
     }
-);
+    );
 }
 }   
 }
